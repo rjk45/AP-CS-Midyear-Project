7d11f7acfe7e709330324ca6a2edc27674eb8c81
rjk45 <riajkamat@gmail.com>
2024-01-12 11:29:04 -0500

    jump class should be done - need to trouble shoot perhaps

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const dino = document.getElementById("dino");
 
 function jump() {
+    if (dino.classList != "jump") {
     dino.classList.add("jump");
+
+    setTimeout(function (){
+        dino.classList.remove('jump');
+    }, 300)
+}
 }
 
 document.addEventListener("keydown", function(event) {
