ea24f69f9914ad420b187dfbe98c08772c0e4315
rjk45 <riajkamat@gmail.com>
2024-01-16 12:49:06 -0500

    trying to fix jump code to make it avoid obstackle and then jump with space bar
    Co-authored-by: Chen_Andrea <andche25@bergen.org>

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const dino = document.getElementById("dino");
-
+const cactus = document.getElementById("cactus");
 function jump() {
     if (dino.classList != "jump") {
     dino.classList.add("jump");
@@ -10,7 +10,21 @@ function jump() {
 }
 }
 
+let isAlive = setInterval(function () {
+    //dino pos + obstacles
 
+    //position of dinosaur on y axis
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    
+    //position of cactus on x axis
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+
+    //collisions --> is alive set to false
+    //if dino is 50 px, then uses that measurment to see contact with the cactus
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop <= 140){ //check if statement to makes sure
+        alert("Game Over!"); //add score and time here at the end too
+    }
+}, 10)
 
 document.addEventListener("keydown", function(event) {
     jump();
